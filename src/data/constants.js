ece2683a785cd782851a7f626c950f75ea152623
imvinay10 <mahajan.vinay10@gmail.com>
2025-06-13 19:24:48 +0530

    Add updated resume PDF and correct LinkedIn link in Bio constants

--- src/data/constants.js
+++ src/data/constants.js
@@ -4,8 +4,8 @@ export const Bio = {
     description:
         "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/imvinay10",
-    resume: require('../assets/VinayMahajan-20250414.pdf'),
-    linkedin: "https://www.linkedin.com/in/vinay-mahajan10/",
+    resume: require('../assets/VinayMahajan_CV.pdf'),
+    linkedin: "https://www.linkedin.com/in/vinaymahajan01/",
     insta: "https://www.instagram.com/i_m.vinay/",
 };
 
